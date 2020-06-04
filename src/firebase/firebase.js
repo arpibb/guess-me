1fac3e4cf7fb4d968d8c8f9391a6ebcfc1875a24
Arpad Bencze <bencze.arpad.21@gmail.com>
2020-06-04 18:54:27 +0200

    fix

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,24 +1,19 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+import dotenv from 'dotenv';
+require('dotenv').config()
 
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
   databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
+  projectId: "guess-me-426ec",
   storageBucket: process.env.STORAGE_BUCKET,
   messagingSenderId: process.env.MESSAGING_SENDER_ID,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID
-  // apiKey: "AIzaSyCWFUrtuda0YyBH8gYIYrVPUin-4P1ic6w",
-  // authDomain: "guess-me-426ec.firebaseapp.com",
-  // databaseURL: "https://guess-me-426ec.firebaseio.com",
-  // projectId: "guess-me-426ec",
-  // storageBucket: "guess-me-426ec.appspot.com",
-  // messagingSenderId: "613302270375",
-  // appId: "1:613302270375:web:2631203aa0c7556af09e82",
-  // measurementId: "G-Z4J5Z8V56E"
+  
 };
 
 export const myFirebase = firebase.initializeApp(firebaseConfig);
